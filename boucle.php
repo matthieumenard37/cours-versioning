8392523c58c260ca20abe69ddc792c4e3fffa99f
matthieumenard37 <matthieu.menard@viacesi.fr>
2019-01-07 10:25:17 +0100

    Premier commit

---
+++ boucle.php
@@ -0,0 +1,11 @@
+<?php
+
+$repetitions = 0;
+
+for ($repetitions = 0 ; $repetitions < 10 ; $repetitions ++)
+{
+	echo '<p>Je ne dois pas copier sur mon voisin '.$repetitions.' fois !</p>';
+}
+
+
+?>
